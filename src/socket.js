964e80977f6780600b249dc90d62107af81a33a2
charan0318be <charanreddyma910@gmail.com>
2025-10-28 13:55:32 +0530

    fixed URL line break error

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,6 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("https://hrms-project-1-eca3.onrender.com
-"); // backend URL
+const socket = io("https://hrms-project-1-eca3.onrender.com"); // backend URL
 
 export default socket;
